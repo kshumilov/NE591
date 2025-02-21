da98e25a6126e9ec022ff81ac2bfed790942c833
Kirill Shumilov <shumilov.kirill98@gmail.com>
2025-02-21 16:07:17 -0500

    submission script fixes

--- submissions/shumilov_inlab07/submit_inlab07.sh
+++ submissions/shumilov_inlab07/submit_inlab07.sh
@@ -3,12 +3,12 @@
 #BSUB -J shumilov_inlab07
 #BSUB -o stdout.%J
 #BSUB -e stderr.%J
-#BSUB -W 00:1
-#BSUB -n 2
+#BSUB -W 00:10
+#BSUB -n 4
 
 module purge
 module load cmake/3.24
 module load gcc/13.2.0
 module load mpi/mpich-x86_64
 
-mpirun -n 2 ./shumilov_inlab07
+mpirun ./shumilov_inlab07

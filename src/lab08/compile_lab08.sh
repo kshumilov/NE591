d237875730906946788f8e171ba110f5611e60bd
Kirill Shumilov <shumilov.kirill98@gmail.com>
2025-03-06 22:20:13 -0500

    Compilation script update

--- src/lab08/compile_lab08.sh
+++ src/lab08/compile_lab08.sh
@@ -6,5 +6,5 @@ module load gcc/13.2.0
 module load mpi/mpich-x86_64
 
 # rm -rf build/
-cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release -DMPI_CXX_COMPILER=mpic++
+cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release --fresh
 cmake --build build/ -- -j

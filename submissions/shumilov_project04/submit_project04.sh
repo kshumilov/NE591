808c106329bf9c3c9e5f4a395ba5cc409c1fe2a5
Kirill Shumilov <shumilov.kirill98@gmail.com>
2025-03-20 20:58:23 -0400

    Better Timing

--- submissions/shumilov_project04/submit_project04.sh
+++ submissions/shumilov_project04/submit_project04.sh
@@ -11,4 +11,4 @@ module load cmake/3.24
 module load gcc/13.2.0
 module load mpi/mpich-x86_64
 
-mpirun -n 9 ./shumilov_project04 examples/s12_pj.inp examples/s12_pj.out
+mpirun -n 9 ./shumilov_project04 examples/s12_pj.inp 1> examples/s12_pj.out

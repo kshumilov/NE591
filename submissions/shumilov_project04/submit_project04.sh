fe529d96c6aebf04246e345e67c95e32f33e589c
Kirill Shumilov <shumilov.kirill98@gmail.com>
2025-03-20 20:56:31 -0400

    update submit for proj04

--- submissions/shumilov_project04/submit_project04.sh
+++ submissions/shumilov_project04/submit_project04.sh
@@ -4,11 +4,11 @@
 #BSUB -o stdout.%J
 #BSUB -e stderr.%J
 #BSUB -W 00:10
-#BSUB -n 4
+#BSUB -n 9
 
 module purge
 module load cmake/3.24
 module load gcc/13.2.0
 module load mpi/mpich-x86_64
 
-mpirun -n 4 ./shumilov_outlab08 examples/s12_pj.inp 1> examples/s12_pj.out
+mpirun -n 9 ./shumilov_project04 examples/s12_pj.inp examples/s12_pj.out
